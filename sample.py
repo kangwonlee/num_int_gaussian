fca5a4a6f1e31b3145e86b63aab8a5a97845b593
KangWon LEE <kangwon@gmail.com>
2024-05-09 15:15:42 +0900

    implement sample.py

--- sample.py
+++ sample.py
@@ -1,21 +1,32 @@
 import random
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 import numerical_integration
 
 
 def sample_main():
     x_begin = 0
-    x_end = 1000
+    x_end_array = np.logspace(3, 6)
 
     n = random.randint(100, 300)
 
-    result_2 = numerical_integration.gauss_int_2(x_begin, x_end, n)
+    area_list = []
 
-    a_array_2 = result_2['a_array_2']
-    i = random.randint(0, len(a_array_2)-1)
-    area_parabola_i = a_array_2[i]
+    for x_end in x_end_array:
+        result_2 = numerical_integration.gauss_int_2(x_begin, x_end, n)
 
-    print(f"area of parabola #{i} is {area_parabola_i}")
+        area_list.append(result_2['area_2'])
+
+    area_array = np.array(area_list)
+    going_where = (area_array * 2) ** 2
+
+    plt.semilogx(x_end_array, going_where)
+    plt.xlabel('integration range end point')
+    plt.ylabel('((Gaussisan integral) * 2) ** 2')
+    plt.grid(True)
 
 
 if "__main__" == __name__:
